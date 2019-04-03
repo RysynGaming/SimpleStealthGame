b81b6e7e6e8b47dbcfa8996990d88f527c2760e4
Rysyn <rysyn.gaming@gmail.com>
2019-04-03 15:25:07 -0700

    added a guard and a path to follow

---
+++ Assets/Scipts/Guard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    public Transform pathHolder;
+
+    void Start()
+    {
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+
+        foreach (Transform waypoint in pathHolder)
+        {
+            Gizmos.DrawCube(waypoint.position, Vector3.one * 0.3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+    }
+}
